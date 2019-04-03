f55503f359a43697befc5c6c012d0f8fd6fa75a9
Joel Hawksley <joel@hawksley.org>
2019-04-03 14:08:21 -0600

    remove default scope on Issue

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -1,8 +1,6 @@
 class Issue < ApplicationRecord
   belongs_to :pull_request, inverse_of: :issue, optional: true
 
-  default_scope { where(pull_request: nil)}
-
   def open?
     state == "open"
   end
