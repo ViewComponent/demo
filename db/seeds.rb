15f1c953c96cc8167b43f3d95e96543b8f4c9ac8
Joel Hawksley <joel@hawksley.org>
2019-04-03 14:03:47 -0600

    add Issue/PullRequest seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Create Issues
+Issue.create(state: "open", title: SecureRandom.hex)
+Issue.create(state: "closed", title: SecureRandom.hex)
+
+# Create Pull Requests
+Issue.create(state: "open", title: SecureRandom.hex, pull_request: PullRequest.create)
+Issue.create(state: "open", title: SecureRandom.hex, pull_request: PullRequest.create(draft: true))
+Issue.create(state: "closed", title: SecureRandom.hex, pull_request: PullRequest.create)
+Issue.create(state: "closed", title: SecureRandom.hex, pull_request: PullRequest.create(merged_at: DateTime.now))
+Issue.create(state: "closed", title: SecureRandom.hex, pull_request: PullRequest.create(draft: true))
