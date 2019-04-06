44935f403269b8ca951d92c93f0d7d5bda268e0b
Joel Hawksley <joel@hawksley.org>
2019-04-05 22:37:27 -0600

    All component tests pass with original partial dropped in

--- app/components/issues/badge.rb
+++ app/components/issues/badge.rb
@@ -2,8 +2,8 @@ module Issues
   class Badge
     include OcticonsHelper
 
-    def initialize(issue:, pull_request:)
-      @issue = issue
+    def initialize(issue:, pull_request: nil)
+      @issue, @pull_request = issue, pull_request
     end
 
     def render
@@ -15,7 +15,23 @@ module Issues
 
     def template
       <<-erb
-      <% if @issue.closed? %>
+      <% if @pull_request && @pull_request.merged? %>
+        <div class="State State--purple">
+          <%= octicon('git-merge') %> Merged
+        </div>
+      <% elsif @pull_request && @pull_request.closed? %>
+        <div class="State State--red">
+          <%= octicon('git-pull-request') %> Closed
+        </div>
+      <% elsif @pull_request && @pull_request.draft? %>
+        <div class="State">
+        <%= octicon('git-pull-request') %> Draft
+        </div>
+      <% elsif @pull_request %>
+        <div class="State State--green">
+          <%= octicon('git-pull-request') %> Open
+        </div>
+      <% elsif @issue.closed? %>
         <div class="State State--red">
           <%= octicon('issue-closed') %> Closed
         </div>
