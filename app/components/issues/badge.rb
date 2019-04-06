1e43bd4a08caa9ab2ee04add8b9b0d20da05615c
Joel Hawksley <joel@hawksley.org>
2019-04-06 12:05:04 -0600

    Decouple Issues::State from AR

--- app/components/issues/badge.rb
+++ app/components/issues/badge.rb
@@ -2,22 +2,44 @@ module Issues
   class Badge < ActionView::Component
     include OcticonsHelper
 
-    def initialize(issue:)
-      @issue = issue
+    STATES = {
+      open: {
+        color: :green,
+        octicon_name: "issue-opened",
+        label: "Open"
+      },
+      closed: {
+        color: :red,
+        octicon_name: "issue-closed",
+        label: "Closed"
+      }
+    }.freeze
+
+    attr_reader :state
+    validates :state, inclusion: { in: STATES.keys }
+
+    def initialize(state:)
+      @state = state
     end
 
     def template
       <<-erb
-      <% if @issue.closed? %>
-        <%= render Primer::State, color: :red, title: "Status: Closed" do %>
-          <%= octicon('issue-closed') %> Closed
-        <% end %>
-      <% else %>
-        <%= render Primer::State, color: :green, title: "Status: Open" do %>
-          <%= octicon('issue-opened') %> Open
-        <% end %>
+      <%= render Primer::State, color: color, title: "Status: Closed" do %>
+        <%= octicon(octicon_name) %> <%= label %>
       <% end %>
       erb
+    end
+
+    def color
+      STATES[state][:color]
+    end
+
+    def octicon_name
+      STATES[state][:octicon_name]
+    end
+
+    def label
+      STATES[state][:label]
     end
   end
 end
