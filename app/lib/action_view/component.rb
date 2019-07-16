0a645e8e54f085f8a82457cba6e73d7e4f33842e
Joel Hawksley <joel@hawksley.org>
2019-07-16 14:01:28 -0600

    add test ensuring components implement initializers

--- app/lib/action_view/component.rb
+++ app/lib/action_view/component.rb
@@ -48,8 +48,6 @@ module ActionView
       call
     end
 
-    def initialize(*); end
-
     class << self
       def inherited(child)
         child.include Rails.application.routes.url_helpers unless child < Rails.application.routes.url_helpers
@@ -88,7 +86,7 @@ module ActionView
       def template_file_path
         filename = self.instance_method(:initialize).source_location[0]
 
-        raise NotImplementedError.new("Subclasses of ActionView::Component must implement #initialize") if filename == __FILE__
+        raise NotImplementedError.new("#{self} must implement #initialize.") unless filename.include?(self.name.underscore)
 
         filename_without_extension = filename[0..-(File.extname(filename).length + 1)]
         siblings_files = Dir["#{filename_without_extension}.*"] - [filename]
