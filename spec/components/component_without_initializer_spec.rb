df46dcadebfa8a5f26b20f8c3de99a78f91a45de
Joel Hawksley <joel@hawksley.org>
2019-07-17 14:49:52 -0600

    update test message

--- spec/components/component_without_initializer_spec.rb
+++ spec/components/component_without_initializer_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
 RSpec.describe ComponentWithoutInitializer do
-  it "responds with a friendly message if there is no template" do
+  it "responds with a friendly message if there is no initializer" do
     error = assert_raises NotImplementedError do
       render_component(ComponentWithoutInitializer.new)
     end
